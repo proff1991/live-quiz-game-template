5cde9843c2a985b25ae59defd62d5669a8066cad
Oleksandr Parkhomenko <119164331+proff1991@users.noreply.github.com>
2026-03-28 00:38:47 +0200

    feat: add the first workable websocket connection 	modified:   server/src/index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,16 +2,34 @@ import { WebSocketServer } from 'ws';
 
 
 var PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
-console.log("Hello");
+console.log(`Hello, websocket started at the PORT: ${PORT}`);
 // WebSocket server
 var wss = new WebSocketServer({ port: PORT });
 
 wss.on('connection', (ws) => {
-  console.log('Client connected');
+    console.log('Client connected');
 
-  ws.on('message', (message) => {
-    var parsed = JSON.parse(message.toString());
+    console.log(ws)
 
-    console.log(parsed);
-  });
+    ws.on('message', (message) => {
+        try {
+            var parsed = JSON.parse(message.toString());
+
+            ws.send(`Your data is here: ${message.toString()}`);
+
+            console.log(parsed);
+        } catch (err) {
+            console.log('Invalid JSON');
+
+            ws.send(JSON.stringify({
+                type: 'error',
+                data: {
+                    message: 'Invalid JSON'
+                },
+                id: 0
+            }));
+        }
+    });
+
+    ws.on('error', console.error);
 });
